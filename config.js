7f368be8931e1de598ddb16ff5dd8dc74573bdab
IceDevx0 <67109507+IceDevx0@users.noreply.github.com>
2020-07-11 18:19:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzMxNTQxNTQ1MzU4MTk2NzU2.Xwnjig.CEIWDEr4UvuUjGaM3Kf5kD__pgE',
+    YT_API_KEY: 'api ', 
+    prefix: '$',
+    devs: ['577881677687160842']
+}
---
+++ server.js
@@ -0,0 +1,116 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://epickingdom.glitch.me/`);
+}, 280000);
+ 
+// كل البكجات الي ممكن تحتجها في اي بوت
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require('discord.js');
+const {TOKEN, YT_API_KEY, prefix, devs} = require('./config')
+const client = new Client({ disableEveryone: true})
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const botversion = require('./package.json').version;
+const simpleytapi = require('simple-youtube-api')
+const moment = require("moment");
+const fs = require('fs');
+const util = require("util")
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require('snekfetch');
+const guild = require('guild');
+const dateFormat = require('dateformat');//npm i dateformat
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(TOKEN);
+const queue = new Map();
+var table = require('table').table
+const Discord = require('discord.js');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {                      
+    if(!message.channel.guild) return;
+       if(message.content.startsWith(prefix + 'tic')) {
+        let modlog = client.channels.find('name', 'tic');
+       if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+       message.channel.sendMessage(`اضغط على الصح لفتح تكت `).then(msg => {
+       
+       
+        msg.react('✅')
+       .then(() => msg.react('✅'))
+     
+     
+ 
+       let activeFilter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+     
+       let active = msg.createReactionCollector(activeFilter, { time: 15000 });
+     
+                                                       
+                               active.on("collect", r => {
+                                  if (
+      message.guild.channels.exists(
+        "name",
+        `ticket-${message.author.id}`
+      )
+    )
+      return;
+    message.guild
+      .createChannel(`ticket-${message.author.id}`, "text")
+      .then(c => {
+        let role = message.guild.roles.find("name", "support");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        let role3 = message.guild.roles.find("name", "support new");
+        c.overwritePermissions(role, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+          SEND_MESSAGES: false,
+          READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true
+        });
+        c.overwritePermissions(role3, {
+          SEND_MESSAGES: true,
+          READ_MESSAGES: true
+        });
+        
+      
+        const embed = new Discord.RichEmbed()
+          .setColor(0xcf40fa)
+          .addField(
+            `Hey ${message.author.username}!`,
+            `الكلام يلي يجي بل تكت`
+          )
+          .setTimestamp();
+        c.send({
+          embed: embed
+        }
+              )
+                                   })
+                                   })
+       }
+                                                                    
+                                   );
+       }
+}
+          )
